61dec972738a52604149c1ce09e38f965e4bb430
DinuComendant <comendantdinu@gmail.com>
2023-11-30 20:19:01 +0200

    Fetch data from API

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Game } from "./Hooks/useGames";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card>
+      <Image src={game.background_image}></Image>{" "}
+      <CardBody>
+        <Heading> {game.name} </Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
